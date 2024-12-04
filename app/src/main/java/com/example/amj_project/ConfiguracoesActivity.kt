72138c8c43ec8789991222c92335e0ee9642b70c
PC <rafamarcucci@gmail.com>
2024-12-04 13:08:39 -0300

    teste de API

--- app/src/main/java/com/example/amj_project/ConfiguracoesActivity.kt
+++
@@ -1,6 +0,0 @@
-class ConfiguracoesActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_configuracoes)
-    }
-}
